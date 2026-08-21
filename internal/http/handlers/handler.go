00dbb36ed18c0c46ce62cab17f57792344ff09d5
Alexander Timonin <2sdaiwxet@gmail.com>
2026-08-21 20:04:34 +0500

    add response helper methods

--- internal/http/handlers/handler.go
+++ internal/http/handlers/handler.go
@@ -1,12 +1,36 @@
 package handlers
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type ErrorResponse struct {
 	Errors string `json:"errors"`
 }
 
-type ApiHandler struct {
+type IAuthService interface {
+	Authenticate(username, password string) (string, error)
 }
 
-func NewApiHandler() *ApiHandler {
-	return &ApiHandler{}
+type ApiHandler struct {
+	auth IAuthService
+}
+
+func NewApiHandler(auth IAuthService) *ApiHandler {
+	return &ApiHandler{
+		auth: auth,
+	}
+}
+
+func respondWithJson(w http.ResponseWriter, status int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(payload)
+}
+
+func respondWithError(w http.ResponseWriter, status int, message string) {
+	respondWithJson(w, status, ErrorResponse{
+		Errors: message,
+	})
 }
