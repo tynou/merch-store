ae0b71d89cfd3f8575516e69da8f03cec00328f1
Alexander Timonin <2sdaiwxet@gmail.com>
2026-08-21 18:05:11 +0500

    route paths to handler methods

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,7 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/tynou/avito-assignment/internal/http/handlers"
 )
 
 func main() {
@@ -46,12 +47,14 @@ func main() {
 		port = "8080"
 	}
 
+	apiHandler := handlers.NewApiHandler()
+
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /hello", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"status": "OK"}`))
-	})
+	router.HandleFunc("GET /api/info", apiHandler.Info)
+	router.HandleFunc("POST /api/sendCoin", apiHandler.SendCoin)
+	router.HandleFunc("GET /api/buy/{item}", apiHandler.Buy)
+	router.HandleFunc("POST /api/auth", apiHandler.Auth)
 
 	log.Printf("server running on port: %s\n", port)
 	if err := http.ListenAndServe(":"+port, router); err != nil {
