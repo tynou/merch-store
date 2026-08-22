f9f62cc84d318c00eaf6e71c0c827c9c2f30f696
Alexander Timonin <2sdaiwxet@gmail.com>
2026-08-22 13:28:12 +0500

    fix init migration

--- db/migrations/001_init.up.sql
+++ db/migrations/001_init.up.sql
@@ -33,14 +33,14 @@ CREATE TABLE IF NOT EXISTS coin_transfers (
 );
 
 INSERT INTO merch (name, price) VALUES
-("t-shirt", 80),
-("cup, 20"),
-("book", 50),
-("pen", 10),
-("powerbank", 200),
-("hoody", 300),
-("umbrella", 200),
-("socks", 10),
-("wallet", 50),
-("pink-hoody", 500)
+('t-shirt', 80),
+('cup', 20),
+('book', 50),
+('pen', 10),
+('powerbank', 200),
+('hoody', 300),
+('umbrella', 200),
+('socks', 10),
+('wallet', 50),
+('pink-hoody', 500)
 ON CONFLICT (name) DO NOTHING;
