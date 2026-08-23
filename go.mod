961fc0bf22c01b6726c3eda7482e91ee2193c561
Alexander Timonin <2sdaiwxet@gmail.com>
2026-08-23 13:39:14 +0500

    create db mocks & info service tests

--- go.mod
+++ go.mod
@@ -9,11 +9,13 @@ require (
 	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/jackc/pgx/v5 v5.10.0
 	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.11.1
 	golang.org/x/crypto v0.55.0
 )
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -23,7 +25,9 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rogpeppe/go-internal v1.16.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/sync v0.22.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.41.0 // indirect
---
+++ internal/service/info/info_test.go
@@ -0,0 +1,84 @@
+package info
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tynou/avito-assignment/internal/apperr"
+	"github.com/tynou/avito-assignment/internal/db"
+	"github.com/tynou/avito-assignment/internal/db/mocks"
+)
+
+func TestGetUserInfo_Success(t *testing.T) {
+	ctx := context.Background()
+	userID := int32(1)
+
+	mockStore := mocks.NewMockQuerier(t)
+
+	mockStore.On("GetUserBalance", ctx, userID).Return(int32(1000), nil)
+
+	mockStore.On("GetUserInventory", ctx, userID).Return([]db.GetUserInventoryRow{
+		{Type: "t-shirt", Quantity: 2},
+		{Type: "book", Quantity: 1},
+	}, nil)
+
+	mockStore.On("GetReceivedTransfers", ctx, userID).Return([]db.GetReceivedTransfersRow{
+		{FromUser: "user1", Amount: 100},
+	}, nil)
+
+	mockStore.On("GetSentTransfers", ctx, userID).Return([]db.GetSentTransfersRow{
+		{ToUser: "user2", Amount: 50},
+		{ToUser: "user3", Amount: 67},
+	}, nil)
+
+	service := NewInfoService(mockStore, mockStore, mockStore)
+
+	resp, err := service.GetInfo(ctx, userID)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, int32(1000), resp.Coins)
+	assert.Len(t, resp.Inventory, 2)
+	assert.Equal(t, "t-shirt", resp.Inventory[0].Type)
+	assert.Equal(t, int32(2), resp.Inventory[0].Quantity)
+	assert.Len(t, resp.CoinHistory.Received, 1)
+	assert.Equal(t, "user1", resp.CoinHistory.Received[0].FromUser)
+	assert.Equal(t, int32(100), resp.CoinHistory.Received[0].Amount)
+	assert.Len(t, resp.CoinHistory.Sent, 2)
+}
+
+func TestGetUserInfo_GetUserBalance_Failure(t *testing.T) {
+	ctx := context.Background()
+	userID := int32(1)
+
+	mockStore := mocks.NewMockQuerier(t)
+
+	mockStore.On("GetUserBalance", ctx, userID).Return(int32(0), apperr.ErrNotFound)
+
+	service := NewInfoService(mockStore, mockStore, mockStore)
+
+	resp, err := service.GetInfo(ctx, userID)
+
+	assert.Nil(t, resp)
+	assert.Error(t, err)
+}
+
+func TestGetUserInfo_GetUserInventory_Failure(t *testing.T) {
+	ctx := context.Background()
+	userID := int32(1)
+
+	mockStore := mocks.NewMockQuerier(t)
+
+	mockStore.On("GetUserBalance", ctx, userID).Return(int32(1000), nil)
+
+	mockStore.On("GetUserInventory", ctx, userID).Return([]db.GetUserInventoryRow{}, errors.New("some error"))
+
+	service := NewInfoService(mockStore, mockStore, mockStore)
+
+	resp, err := service.GetInfo(ctx, userID)
+
+	assert.Nil(t, resp)
+	assert.Error(t, err)
+}
